3a38cee410deae9cc11028c114f2043890eb001b
Esteban Flamini <38509691+estebanflamini@users.noreply.github.com>
2023-01-14 18:53:10 -0300

    Upgraded version number to 1.0

--- efabrules.py
+++ efabrules.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "0.99"
+__version__ = "1.0"
 
 import gettext
 import os
